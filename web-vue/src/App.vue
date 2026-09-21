cf9e9b3a7303864943d902fb8ea00f8944877abe
Daniil Gurski <daniilgurski6@gmail.com>
2026-09-21 17:13:42 +0200

    feat: remove styles, unsued routes

--- web-vue/src/App.vue
+++ web-vue/src/App.vue
@@ -16,8 +16,6 @@ import { RouterLink, RouterView } from "vue-router";
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/guides">Guides</RouterLink>
       </nav>
     </div>
